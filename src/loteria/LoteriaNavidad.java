3ea3bf7b1279f678f6e77972fc1cea8fa4b2bd7f
Samuel <>
2023-01-16 12:28:57 +0100

    Mensaje loteria

--- src/loteria/LoteriaNavidad.java
+++ src/loteria/LoteriaNavidad.java
@@ -1,7 +1,48 @@
 package loteria;
 
+import java.util.Scanner;
+
 public class LoteriaNavidad {
-	public static void Main(String[] args) {
-		          System.out.println("a");
-	}
+ static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int opcio = 0;
+        boolean creado = false;
+        
+        while (opcio != 3) {
+            System.out.println("Que vols fer? Escull una opcio");
+            System.out.println("1.Inicia simulacio");
+            System.out.println("2.Buscar el premi");
+            System.out.println("3.Sortir");
+            opcio = comprobarEnter();
+
+            switch (opcio) {
+
+                case 1:
+                    creado = Simulacion.IniciarSimulacion();
+                    if (creado = true) {
+                        System.out.println("Simulacion creada");
+                    } else {
+                        System.out.println("No se ha podido crear la simulacion");
+                    }
+                    break;
+
+                case 2:
+                    
+                    if (creado = true) {
+                        System.out.println("Introdueix el numero a comprobar");
+                        int numero=scan.nextInt();
+                        System.out.println(busquedapremios.comprobar(numero,Simulacion.getPremios));
+                    } else {
+                        System.out.println("Porfavor, inicia una nueva simulacion");
+                    }
+
+                    break;
+
+            }
+        }
+        System.out.println("Adeu, fins un altre");
+
+    }
 }
