b2abd3dd83f71f0937c9749822bea11b6d8c24e0
ausias <ausias@hINFO6-28.xiam.cat>
2023-01-24 08:30:57 +0100

    Funciones de BusquedaPremios

--- src/loteria/BusquedaPremios.java
+++ src/loteria/BusquedaPremios.java
@@ -4,7 +4,32 @@ import java.util.ArrayList;
 
 public class BusquedaPremios {
 
-    public static String Comprobar(int numero, ArrayList<Premio> Premiados) {
+	public static String Comprobar(int numero, ArrayList<Premio> Premiados, double importe) {
+    	int suma = ComprobarPremio(numero, Premiados);
+    	suma += ComprobarAproximaciones(int numero, ArrayList<Premio> Premiados);
+    	suma += ComprobarCentenas(int numero, ArrayList<Premio> Premiados);
+    	suma += ComprobarDosUltimas(int numero, ArrayList<Premio> Premiados);
+    	CalcularTotal(int suma, double importe);
         return null;
     }
+
+	private static int ComprobarPremio(int numero, ArrayList<Premio> Premiados) {
+
+	}
+
+	private static int ComprobarAproximaciones(int numero, ArrayList<Premio> Premiados) {
+
+	}
+
+	private static int ComprobarCentenas(int numero, ArrayList<Premio> Premiados) {
+
+	}
+
+	private static int ComprobarDosUltimas(int numero, ArrayList<Premio> Premiados) {
+
+	}
+
+	private static double CalcularTotal(int suma, double importe) {
+
+	}
 }
