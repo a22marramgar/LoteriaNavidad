9aa5911534cf58275739b70da6e0344000ab4ce5
Mario <ausias@hINFO6-28.xiam.cat>
2023-02-22 13:26:00 +0100

    mostrarTablaMember a medias, Membre get añadidos

--- src/loteria/Colla.java
+++ src/loteria/Colla.java
@@ -14,10 +14,10 @@ public class Colla {
     private String _nomColla;
     private int _nombreMembres;
     private double _importTotal;
-    private String _anyJugat;
+    private int _anyJugat;
     private ArrayList<Membre> _lista;
 
-    public Colla(String nomColla, String anyJugat) {
+    public Colla(String nomColla, int anyJugat) {
         this._nomColla = nomColla;
         this._anyJugat = anyJugat;
         this._nombreMembres = 0;
@@ -36,15 +36,45 @@ public class Colla {
     public boolean afegirMembre(String nomMembre, String numeroJugat, double importMembre) {
         boolean afegit = false;
         Membre nouMembre = new Membre(nomMembre, numeroJugat, importMembre);
-        if (!ComprobarRepetido(nouMembre)) {
+        if (!comprobarRepetido(nouMembre)) {
             this._lista.add(nouMembre);
+            this._nombreMembres += 1;
+            this._importTotal += importMembre;
             afegit = true;
         }
 
         return afegit;
     }
     
-    private boolean ComprobarRepetido(Membre nouMembre) {
+    private boolean comprobarRepetido(Membre nouMembre) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public void mostrar(){
+        System.out.println("Colla: "+this._nomColla);
+        System.out.println("Any: "+this._anyJugat);
+        System.out.println("Diners: "+this._importTotal);
+        System.out.println("Premi: "+calcularPremioTotal());
+        System.out.println();
+        System.out.println("MEMBRES");
+        mostrarTablaMembres();
+    }
+
+    private double calcularPremioTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void mostrarTablaMembres() {
+        System.out.println("+------------------+--------+--------+-----------+");
+        System.out.println("|       NOM        | NUMERO | DINERS |   PREMI   |");
+        System.out.println("+------------------+--------+--------+-----------+");
+        Comprobacio com = new Comprobacio(ArchivosBinarios.CargarLista(this._anyJugat));
+        for (Membre membre : this._lista) {
+            System.out.print("| "+String.format("%-17s", membre.getNom())
+            +"| "+membre.getNumero()+" | "+String.format("%7s-", membre.getImport())
+            +"| "+String.format(_"%-7s",com.Comprobar(membre.getNumero(), 200f))));
+        }
+        System.out.println("+------------------+--------+--------+-----------+");
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
--- src/loteria/Membre.java
+++ src/loteria/Membre.java
@@ -13,5 +13,17 @@ class Membre {
     Membre(String nomMembre, String numeroJugat, double importMembre) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    public String getNom() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getNumero() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public double getImport() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
