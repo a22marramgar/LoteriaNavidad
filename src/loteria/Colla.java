62f155fa8ebd4d4d5c5fc4fb9e565f41c34ef247
Mario <ausias@hINFO6-28.xiam.cat>
2023-02-22 10:57:24 +0100

    AfegirMembre finished, Membre class añadida

--- src/loteria/Colla.java
+++ src/loteria/Colla.java
@@ -1,28 +1,50 @@
 /**
- * 
+ *
  */
 package loteria;
+
+import java.util.ArrayList;
 
 /**
  * @author ausias
  *
  */
 public class Colla {
-	private String _nomColla;
-	private int _nombreMembres;
-	private double _importTotal;
-	private String _anyJugat;
-	
-	public Colla(String nomColla, String anyJugat) {
-		this._nomColla = nomColla;
-		this._anyJugat = anyJugat;
-		this._nombreMembres = 0;
-		this._importTotal = 0;
-	}
-	
-	public boolean afegirMembre(String nomMembre, String numeroJugat, double importMembre) {
-		boolean afegit = false;
-		
-		return afegit;
-	}
+
+    private String _nomColla;
+    private int _nombreMembres;
+    private double _importTotal;
+    private String _anyJugat;
+    private ArrayList<Membre> _lista;
+
+    public Colla(String nomColla, String anyJugat) {
+        this._nomColla = nomColla;
+        this._anyJugat = anyJugat;
+        this._nombreMembres = 0;
+        this._importTotal = 0;
+    }
+
+    /**
+     * Afegeix un membre a la lista de membres si no existeix el membre amb el
+     * numero jugat a la llista de membres
+     *
+     * @param nomMembre String
+     * @param numeroJugat String
+     * @param importMembre double
+     * @return true si s'ha afegit, false si està repetit
+     */
+    public boolean afegirMembre(String nomMembre, String numeroJugat, double importMembre) {
+        boolean afegit = false;
+        Membre nouMembre = new Membre(nomMembre, numeroJugat, importMembre);
+        if (!ComprobarRepetido(nouMembre)) {
+            this._lista.add(nouMembre);
+            afegit = true;
+        }
+
+        return afegit;
+    }
+    
+    private boolean ComprobarRepetido(Membre nouMembre) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
